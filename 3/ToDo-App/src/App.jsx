ba7e6ee63191771638a13cd5f4dcb4a436aa0c15
Kartik Rane <kartiksrane05@gmail.com>
2024-05-17 12:59:59 +0530

    Complete Both Assignment

--- 3/ToDo-App/src/App.jsx
+++ 3/ToDo-App/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+import { useCallback } from "react"
+import { useState } from "react"
+function App(){
+  const [todo,setTodo] = useState([])
+  const [title,setTitle] = useState("")
+  const [desc,setDesc] = useState("")
+  const ToDoData = todo.map((element,index)=>(
+    <div key={index}>
+      <h4>{element.title}</h4>
+      <h4>{element.description}</h4>
+      <button onClick={()=>{
+        const arr = [...todo]
+        arr[index].status = true
+        setTodo(arr)
+      }}>{element.status == false ? "Mark As Done!!" : "Done!!"}</button>
+    </div>
+  ))
+  const AddTodo = useCallback(()=>{
+      const ToWork = [...todo]
+      ToWork.push({
+        title:title,
+        description:desc,
+        status:false
+      })
+      setTodo(ToWork)
+  },[title,desc])
+  return <>
+    <center>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>ToDo-App</h1>
+        <input type="text" placeholder="Enter Title" onInput={(e)=>setTitle(e.target.value)}/>
+        <br /><br />
+        <input type="text" placeholder="Enter Description" onInput={(e)=>setDesc(e.target.value)}/>
+        <br /><br />
+        <button onClick={AddTodo}>Add ToDo</button>
+        <br /><br />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </center>
+    <div style={{display:"flex"}}>{ToDoData}</div>
+  </>
 }
-
-export default App
+export default App;
